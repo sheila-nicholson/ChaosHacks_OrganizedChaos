0ba9fe9ad5cb10e29aa802d12fbe581f118a5f3e
Justine Gerard <116043724+JustineG57@users.noreply.github.com>
2024-03-02 17:28:00 +0000

    address updated

--- chaos.py
+++ chaos.py
@@ -6,11 +6,11 @@ headers = {"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2V
 url = "https://api.edenai.run/v2/image/object_detection"
 json_payload = {
     "providers": "google,amazon",
-    "file_url": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.msnbc.com%2Fknow-your-value%2Fwhy-cluttered-desk-kills-your-productivity-how-fix-it-n956316&psig=AOvVaw21nrBIpSmw6AH0wtiTrrUP&ust=1709485640703000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCMDbubOI1oQDFQAAAAAdAAAAABAE",
+    "file_url": "https://media-cldnry.s-nbcnews.com/image/upload/t_fit-760w,f_auto,q_auto:best/newscms/2019_02/2706861/190107-messy-desk-stock-cs-910a.jpg",
     "fallback_providers": ""
 }
 
 response = requests.post(url, json=json_payload, headers=headers)
 
 result = json.loads(response.text)
-print(result["google"]["items"])
+print(result["amazon"]["items"])
