3013223d7b18cb86e10850ebfa0f5a4dd219805e
Justine Gerard <116043724+JustineG57@users.noreply.github.com>
2024-03-02 22:13:58 +0000

    webpages

--- chaos.py
+++ chaos.py
@@ -27,41 +27,41 @@ def index():
     # Render the HTML form
     return app.send_static_file('index.html')
 
-@app.route('/index', methods=['POST'])
+@app.route('/process', methods=['POST'])
 def process():
     if request.method == "POST":
         # Get the input data from the request
         file_URL = request.form.get["url"]
         location_label = request.json["location"]
 
-        json_payload = {
-            "providers": "google,amazon",
-            # Messy Desk
-            "file_url": file_URL,
-            "fallback_providers": ""
-        }
+        # json_payload = {
+        #     "providers": "google,amazon",
+        #     # Messy Desk
+        #     "file_url": file_URL,
+        #     "fallback_providers": ""
+        # }
 
-        response = requests.post(api_url, json=json_payload, headers=headers)
+        # response = requests.post(api_url, json=json_payload, headers=headers)
 
-        with open("result.json", "w") as file:
-            json.dump(json.loads(response.text), file)
+        # with open("result.json", "w") as file:
+        #     json.dump(json.loads(response.text), file)
 
-        with open("result.json", "r") as file:
-            data = json.load(file)
+        # with open("result.json", "r") as file:
+        #     data = json.load(file)
 
-        # Print labels from each provider
-        items_dict = OrderedDict()
-        for provider, provider_data in data.items():
-            #print(f"Labels from {provider}:")
+        # # Print labels from each provider
+        # items_dict = OrderedDict()
+        # for provider, provider_data in data.items():
+        #     #print(f"Labels from {provider}:")
 
-            if provider == 'amazon':
-                for item in provider_data["items"]:
-                    items_dict[item['label']] = {'confidence': item['confidence'], 'location': location_label}
-                    #print(item['label'], items_dict[item['label']])
+        #     if provider == 'amazon':
+        #         for item in provider_data["items"]:
+        #             items_dict[item['label']] = {'confidence': item['confidence'], 'location': location_label}
+        #             #print(item['label'], items_dict[item['label']])
 
-        sorted_items_dict = OrderedDict(sorted(items_dict.items()))
-
-        return render_template("result.html", stocks=sorted_items_dict)
+        # sorted_items_dict = OrderedDict(sorted(items_dict.items()))
+        return 
+        #return render_template("index.html", stocks=sorted_items_dict)
 
         #for label, item_data in sorted_items_dict.items():
             #print(label, item_data)
