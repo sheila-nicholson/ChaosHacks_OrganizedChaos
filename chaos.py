e0056344ad914a810c362e62686ca2dfa06c714f
Justine Gerard <116043724+JustineG57@users.noreply.github.com>
2024-03-02 17:51:11 +0000

    dict to json

--- chaos.py
+++ chaos.py
@@ -7,11 +7,14 @@ headers = {"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2V
 url = "https://api.edenai.run/v2/image/object_detection"
 json_payload = {
     "providers": "google,amazon",
+    # Messy Desk
     "file_url": "https://media-cldnry.s-nbcnews.com/image/upload/t_fit-760w,f_auto,q_auto:best/newscms/2019_02/2706861/190107-messy-desk-stock-cs-910a.jpg",
     "fallback_providers": ""
 }
 
 response = requests.post(url, json=json_payload, headers=headers)
 
-result = json.loads(response.text)
-print(result["google"]["items"])
+with open("result.json", "w") as file:
+    json.dump(json.loads(response.text), file)
+
+
